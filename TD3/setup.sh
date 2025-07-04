7f0db04b32f42de55187b5423dccca39010a5d43
meng <1274802317@qq.com>
2025-07-04 13:24:24 +0800

    add mapping and change seed

---
+++ TD3/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# 设置 ROS 环境变量
+export ROS_HOSTNAME=localhost
+export ROS_MASTER_URI=http://localhost:11311
+export ROS_PORT_SIM=11311
+
+# 设置 Gazebo 路径
+export GAZEBO_RESOURCE_PATH=~/Desktop/nav/DRL-robot-navigation/catkin_ws/src/multi_robot_scenario/launch
+
+# 加载 zsh 配置（如果有需要）
+source ~/.zshrc
+
+# 进入工作空间并加载 ROS 环境
+cd ../catkin_ws
+source devel_isolated/setup.zsh
+
+cd ../TD3
--- TD3/test_velodyne_td3.py
+++ TD3/test_velodyne_td3.py
@@ -44,7 +44,7 @@ class TD3(object):
 
 # Set the parameters for the implementation
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # cuda or cpu
-seed = 0  # Random seed number
+seed = 1  # Random seed number
 max_ep = 500  # maximum number of steps per episode
 file_name = "TD3_velodyne"  # name of the file to load the policy from
 
