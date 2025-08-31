36260395808755f5627fb1940f9c8b62b722c086
Shiyong Meng <129972764+xiaowuguiOvO@users.noreply.github.com>
2025-08-31 12:41:11 +0800

    Delete catkin_ws directory

--- catkin_ws/build_isolated/multi_robot_scenario/CMakeFiles/multi_robot_scenario_xacro_generated_to_devel_space_.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for multi_robot_scenario_xacro_generated_to_devel_space_.
--- catkin_ws/devel_isolated/env.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-# generated from catkin.builder Python module
-
-/home/meng/Desktop/exploration/DRL-robot-navigation/catkin_ws/devel_isolated/velodyne_simulator/env.sh "$@"
